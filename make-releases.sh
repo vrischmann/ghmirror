f02b89a4e612a83572f3ae6409defdc313c04bc8
Vincent Rischmann <vincent@vrischmann.me>
2015-09-20 01:09:32 +0200

    set the version and commit in the build

--- make-releases.sh
+++ make-releases.sh
@@ -2,6 +2,7 @@
 
 export GO15VENDOREXPERIMENT=1
 
-gox --os="linux windows darwing openbsd freebsd" \
+gox --ldflags="-X main.commit=$(git rev-parse HEAD) -X main.version=$(cat VERSION)" \
+    --os="linux windows darwing openbsd freebsd" \
     --arch="386 amd64" \
     --output="build/{{.Dir}}_{{.OS}}_{{.Arch}}"

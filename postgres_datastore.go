450c035fc43f917e2d3752bf9f114630a5065da3
Vincent Rischmann <me@vrischmann.me>
2016-03-23 01:15:41 +0100

    datastore: implement the Postgres datastore

--- postgres_datastore.go
+++ postgres_datastore.go
@@ -3,10 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"sync"
 )
 
 type postgresDataStore struct {
 	db *sql.DB
+	mu sync.Mutex
 }
 
 func newPostgresDataStore(conf *postgresConf) (DataStore, error) {
@@ -23,14 +25,87 @@ func (s *postgresDataStore) Close() error {
 	return s.db.Close()
 }
 
-func (s *postgresDataStore) Lock()   {}
-func (s *postgresDataStore) Unlock() {}
-
-func (s *postgresDataStore) Repositories() (res Repositories, err error) {
-
-	return
+func (s *postgresDataStore) Lock() {
+	s.mu.Lock()
 }
 
-func (s *postgresDataStore) GetByID(id int64) (*Repository, error) { return nil, nil }
-func (s *postgresDataStore) HasRepository(id int64) (bool, error)  { return false, nil }
-func (s *postgresDataStore) AddRepository(repo *Repository) error  { return nil }
+func (s *postgresDataStore) Unlock() {
+	s.mu.Unlock()
+}
+
+func (s *postgresDataStore) Repositories() (Repositories, error) {
+	var res Repositories
+
+	const q = `SELECT id, name, local_path, clone_url, hook_id FROM repository`
+
+	rows, err := s.db.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var (
+		id, hookID                int64
+		name, localPath, cloneURL string
+	)
+
+	for rows.Next() {
+		if err := rows.Scan(&id, &name, &localPath, &cloneURL, &hookID); err != nil {
+			return nil, err
+		}
+
+		repo := &Repository{
+			ID:        id,
+			Name:      name,
+			LocalPath: localPath,
+			CloneURL:  cloneURL,
+			HookID:    hookID,
+		}
+
+		res = append(res, repo)
+	}
+
+	return res, nil
+}
+
+func (s *postgresDataStore) GetByID(id int64) (*Repository, error) {
+	const q = `SELECT name, local_path, clone_url, hook_id FROM repository
+               WHERE id = $1`
+
+	var (
+		name, localPath, cloneURL string
+		hookID                    int64
+	)
+
+	err := s.db.QueryRow(q).Scan(&name, &localPath, &cloneURL, &hookID)
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, nil
+	case err != nil:
+		return nil, err
+	}
+
+	repo := &Repository{
+		ID:        id,
+		Name:      name,
+		LocalPath: localPath,
+		CloneURL:  cloneURL,
+		HookID:    hookID,
+	}
+
+	return repo, nil
+}
+
+func (s *postgresDataStore) HasRepository(id int64) (bool, error) {
+	repo, err := s.GetByID(id)
+	return repo != nil, err
+}
+
+func (s *postgresDataStore) AddRepository(repo *Repository) error {
+	const q = `INSERT INTO repository(name, local_path, clone_url, hook_id)
+               VALUES ($1, $2, $3, $4)`
+
+	// TODO(vincent): do we need the last inserted id for something ?
+	_, err := s.db.Exec(q, repo.Name, repo.LocalPath, repo.CloneURL, repo.HookID)
+	return err
+}
